c36c9fce41b9c4b0e5f67f117b8d092898521e6f
HeegyeongJ <144974339+HeegyeongJ@users.noreply.github.com>
2023-10-27 16:20:15 +0900

    장바구니 내 버튼 추가
    - add, remove 버튼

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -4,9 +4,15 @@ import styled from 'styled-components';
 const CartItem = ({item}) => {
     return (
         <ItemDiv>
-            <p>상품: {item.title}</p>
-            <p>담은 개수: {item.quantity}</p>
-            <p>총 금액: {item.totalPrice}</p>
+            <div>
+                <p>상품: {item.title}</p>
+                <p>담은 개수: {item.quantity}</p>
+                <p>총 금액: {item.totalPrice}</p>
+            </div>
+            <div>
+                <Button>-</Button>
+                <Button>+</Button>
+            </div>
         </ItemDiv>
     );
 };
@@ -15,7 +21,16 @@ export default CartItem;
 
 
 const ItemDiv = styled.div`
+    display:flex;
+    justify-content: space-between;
+    align-items: center;
     border: 1px solid #000;
     padding: 10px 15px;
     margin: 10px 0;
+`
+
+const Button = styled.button`
+    display:inline-block;
+    width:30px;
+    height:30px;
 `
